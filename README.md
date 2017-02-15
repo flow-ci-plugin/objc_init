b167bf0c5863b1a411dca84a8e4c2b859f99bbd4
Will <yh@fir.im>
2017-02-15 16:51:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# objc_init Step
+Objc environment initialize
+
+### INPUTS
+
+## EXAMPLE 
+
+```yml
+steps:
+  - name: objc_init
+    enable: true
+    failure: true
+    plugin:
+      name: objc_init
+      inputs:
+```
---
+++ run.sh
@@ -0,0 +1,59 @@
+# ************************************************************
+#
+# those are default envs that you can use them at any step.
+#
+# ************************************************************
+
+echo '===============DEFAULT ENVS======================='
+
+echo "\$FLOW_PROJECT_NAME = $FLOW_PROJECT_NAME"
+echo "\$FLOW_USER_ID = $FLOW_USER_ID"
+echo "\$FLOW_PROJECT_ID = $FLOW_PROJECT_ID"
+echo "\$FLOW_PROJECT_GIT_URL = $FLOW_PROJECT_GIT_URL"
+echo "\$FLOW_PROJECT_LANGUAGE = $FLOW_PROJECT_LANGUAGE"
+echo "\$FLOW_GIT_HOST = $FLOW_GIT_HOST"
+echo "\$FLOW_JOB_ID = $FLOW_JOB_ID"
+echo "\$FLOW_EVENT_ID = $FLOW_EVENT_ID"
+echo "\$FLOW_EVENT_NUMBER = $FLOW_EVENT_NUMBER"
+echo "\$FLOW_PHP_OLD_VERSION = $FLOW_PHP_OLD_VERSION"
+
+echo '=================================================='
+# ************************************************************
+#
+# those are default envs that you can use them at any step.
+#
+# ************************************************************
+export LANG=en_US.UTF-8
+
+echo '===============DEFAULT ENVS======================='
+
+# 证书秘钥
+echo "\$FLOW_IOS_CERTIFICATE_PASS = $FLOW_IOS_CERTIFICATE_PASS"
+
+# 证书路径
+echo "\$FLOW_IOS_P12_FILE = $FLOW_IOS_P12_FILE"
+
+# provision 路径
+echo "\$FLOW_IOS_MOBILEPROVISION_FILE = $FLOW_IOS_MOBILEPROVISION_FILE"
+
+# 证书名称
+echo "\$FLOW_IOS_CODE_SIGN_IDENTITY = $FLOW_IOS_CODE_SIGN_IDENTITY"
+
+sed -i -e '1i\
+StrictHostKeyChecking  no
+' ~/.ssh/config
+
+echo '=================================================='
+
+source $HOME/.rvm/scripts/rvm
+rvm use 2.3.0
+rvm list
+
+# install xcpretty
+# gem install xcpretty
+
+# 证书信息
+security find-identity -p codesigning ~/Library/Keychains/app-build.keychain
+
+# Pods repos
+pod repo list
